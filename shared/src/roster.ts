ce799e2b916365085c6d1da39a5e8a79017652ec
Aaron Suggs <975+ktheory@users.noreply.github.com>
2026-09-21 19:19:32 -0600

    kqiu

--- shared/src/roster.ts
+++ shared/src/roster.ts
@@ -24,7 +24,7 @@ export const PEOPLE: readonly Person[] = [
   { id: "emily.stupar", name: "Emily Stupar", short: "Emily" },
   { id: "jackson.howe", name: "Jackson Howe", short: "Jackson" },
   { id: "jenny.thai", name: "Jenny Thai", short: "Jenny" },
-  { id: "kqiu", name: "Kevin", short: "Kevin" },
+  { id: "kqiu", name: "Kevin Qiu", short: "Kevin" },
   { id: "kyle.cayemittes", name: "Kyle Cayemittes", short: "Kyle" },
   { id: "michelle.trame", name: "Michelle Trame", short: "Michelle" },
   { id: "ryan.dontas", name: "Ryan Dontas", short: "Ryan" },
